95845e1e7c900063e8566b05f029c1efda0d7072
KatieBuc <34525024+KatieBuc@users.noreply.github.com>
2017-12-15 22:40:48 +1100

    Update code.py
    Apparently I can't spell...

--- code.py
+++ code.py
@@ -1,13 +1,13 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib as mpl #using repl.it which has no GUI server to display graphs on, so I have to import this lib
+import matplotlib as mpl #using repl.it, no GUI server to display this on
 mpl.use('Agg')
 
 JSEIndex = []
 JSEJunior = []
 CombIndex = []
-AllJamacian = []
+AllJamaican = []
 JSESelect = []
 USEquities =[]
 ValuesDF = []
@@ -45,16 +45,16 @@ with open('index-history (2).csv') as csvfile:
         
 print(CombIndex)
 
-#All Jamacian
+#All Jamaican
 with open('index-history (3).csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
 
     for row in readCSV:
         myList = row[2]
         myList = float(myList)
-        AllJamacian.append(myList)
+        AllJamaican.append(myList)
         
-print(AllJamacian)
+print(AllJamaican)
 
 #JSE Select
 with open('index-history (4).csv') as csvfile:
@@ -82,7 +82,7 @@ print(USEquities)
 plt.plot(JSEJunior, label="JSE Junior")
 plt.plot(JSEIndex, label="JSE Index")
 plt.plot(CombIndex, label="Combined Index")
-plt.plot(AllJamacian, label="All Jamacian")
+plt.plot(AllJamaican, label="All Jamacian")
 plt.plot(JSESelect, label="JSE Select")
 plt.plot(USEquities, label="US Equities")
 plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
@@ -97,7 +97,7 @@ plt.clf()
 JSEJunior_normed = [i/sum(JSEJunior) for i in JSEJunior]
 JSEIndex_normed = [i/sum(JSEIndex) for i in JSEIndex]
 CombIndex_normed = [i/sum(CombIndex) for i in CombIndex]
-AllJamacian_normed = [i/sum(AllJamacian) for i in AllJamacian]
+AllJamaican_normed = [i/sum(AllJamaican) for i in AllJamaican]
 JSESelect_normed = [i/sum(JSESelect) for i in JSESelect]
 USEquities_normed = [i/sum(USEquities) for i in USEquities]
 #print(JSEJunior_normed)
@@ -106,7 +106,7 @@ USEquities_normed = [i/sum(USEquities) for i in USEquities]
 plt.plot(JSEJunior_normed, label="JSE Junior")
 plt.plot(JSEIndex_normed, label="JSE Index")
 plt.plot(CombIndex_normed, label="Combined Index")
-plt.plot(AllJamacian_normed, label="All Jamacian")
+plt.plot(AllJamaican_normed, label="All Jamaican")
 plt.plot(JSESelect_normed, label="JSE Select")
 plt.plot(USEquities_normed, label="US Equities")
 plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
@@ -123,7 +123,7 @@ ValCorr = np.corrcoef(JSEJunior_normed, JSEIndex_normed)[0, 1]
 StringCorr.append(ValCorr)
 ValCorr = np.corrcoef(JSEJunior_normed, CombIndex_normed)[0, 1]
 StringCorr.append(ValCorr)
-ValCorr = np.corrcoef(JSEJunior_normed, AllJamacian_normed)[0, 1]
+ValCorr = np.corrcoef(JSEJunior_normed, AllJamaican_normed)[0, 1]
 StringCorr.append(ValCorr)
 ValCorr = np.corrcoef(JSEJunior_normed, JSESelect_normed)[0, 1]
 StringCorr.append(ValCorr)
@@ -131,21 +131,21 @@ ValCorr = np.corrcoef(JSEJunior_normed, USEquities_normed)[0, 1]
 StringCorr.append(ValCorr)
 ValCorr = np.corrcoef(JSEIndex_normed, CombIndex_normed)[0, 1]
 StringCorr.append(ValCorr)
-ValCorr = np.corrcoef(JSEIndex_normed, AllJamacian_normed)[0, 1]
+ValCorr = np.corrcoef(JSEIndex_normed, AllJamaican_normed)[0, 1]
 StringCorr.append(ValCorr)
 ValCorr = np.corrcoef(JSEIndex_normed, JSESelect_normed)[0, 1]
 StringCorr.append(ValCorr)
 ValCorr = np.corrcoef(JSEIndex_normed, USEquities_normed)[0, 1]
 StringCorr.append(ValCorr)
-ValCorr = np.corrcoef(CombIndex_normed, AllJamacian_normed)[0, 1]
+ValCorr = np.corrcoef(CombIndex_normed, AllJamaican_normed)[0, 1]
 StringCorr.append(ValCorr)
 ValCorr = np.corrcoef(CombIndex_normed, JSESelect_normed)[0, 1]
 StringCorr.append(ValCorr)
 ValCorr = np.corrcoef(CombIndex_normed, USEquities_normed)[0, 1]
 StringCorr.append(ValCorr)
-ValCorr = np.corrcoef(AllJamacian_normed, JSESelect_normed)[0, 1]
+ValCorr = np.corrcoef(AllJamaican_normed, JSESelect_normed)[0, 1]
 StringCorr.append(ValCorr)
-ValCorr = np.corrcoef(AllJamacian_normed, USEquities_normed)[0, 1]
+ValCorr = np.corrcoef(AllJamaican_normed, USEquities_normed)[0, 1]
 StringCorr.append(ValCorr)
 ValCorr = np.corrcoef(JSESelect_normed, USEquities_normed)[0, 1]
 StringCorr.append(ValCorr)
